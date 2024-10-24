e2b3950094d5955c00f6ff347400ca60f69384be
ankit.Chauhan <ankit.chauhan@capgemini.com>
2024-10-24 09:16:08 +0530

    sample route handler added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 7777
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/user', (req, res) => {
+  res.send({ firstName: " Ankit" , lastName: "chauhan"})
+})
+app.post('/user', (req, res) => {
+  console.log("save data to db")
+  res.send('successfully saved data to database')
 })
 
 app.listen(port, () => {
