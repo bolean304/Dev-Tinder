de9c5a170e868f3728947d1a6c717adf926e1028
Ankit Chauhan <99311193+bolean304@users.noreply.github.com>
2025-07-09 10:42:28 +0530

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () =>{
 await mongoose.connect(
-    "mongodb+srv://user_devtinder:ghe5DLfysPjf0jJq@cluster0.e3bcy.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://user_devtinder/devTinder?retryWrites=true&w=majority&appName=Cluster0"
 )};
 module.exports = {
     connectDB
